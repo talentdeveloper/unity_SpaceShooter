9c5420b238e5b2d979014afcc67660f3073a590f
Jason Ma <jasonma@Jasons-iMac.local>
2019-06-12 17:23:09 +0800

    initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+public class PlayerController : MonoBehaviour
+{
+    private const float V = 0.0f;
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    private Rigidbody rb;
+
+    void Update ()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+//          GameObject clone = 
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(moveHorizontal, V, moveVertical);
+        rb.velocity = movement * speed;
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            V,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(V, V, rb.velocity.x * -tilt);
+    }
+
+}
